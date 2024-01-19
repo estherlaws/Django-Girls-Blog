4bbb48b6ef17c839c7defb6dcea6de173bb092bc
estherlaws <estherelaws@gmail.com>
2024-01-18 23:07:46 -0500

    Initial Commit

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.PROTECT)
     title = models.CharField(max_length=200)
@@ -20,7 +18,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+        
 class Comment(models.Model):
     post = models.ForeignKey(
         'blog.Post', on_delete=models.CASCADE, related_name='comments')
